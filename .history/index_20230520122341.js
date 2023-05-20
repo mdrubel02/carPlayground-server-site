67669195f736808a8077432e7374f756753bc841
mdrubel02 <mdrubel072125@gmail.com>
2023-05-20 12:58:26 +0600

    'basic-setup'

---
+++ .history/index_20230520122341.js
@@ -0,0 +1,20 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+var jwt = require('jsonwebtoken');
+const app = express()
+const port = process.env.PORT || 5000 
+
+app.use(cors())
+app.use(express.json())
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port,()=>{
+    console.log(`carPlayground server is running on ${port}`)
+})
